4cda2ce0f54b1a7097abb4f2636fbca6b2587f3c
Kauã Pereira <borgeskaua1210@gmail.com>
2026-02-26 14:54:50 -0300

    adicionando exemplos de erros de compilação em Java

---
+++ src/praticando/java/basico/ComecandoComJava/Erros.java
@@ -0,0 +1,22 @@
+package praticando.java.basico
+
+public class TesteErros {
+
+	public static void main(String args) {
+		
+		String nome = "Kaua
+		int idade = "19";
+		
+		System.out.println("Nome: " + nome)
+		
+		if (idade = 19) {
+			System.out.println("Idade correta");
+		}
+		
+		int numero = 10
+		int numero = 20;
+		
+		System.out.println(resultado);
+		
+	}
+}
