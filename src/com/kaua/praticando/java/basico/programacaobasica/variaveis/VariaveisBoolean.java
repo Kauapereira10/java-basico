686b8261fc7e7252eab779d91e93e9514c1df345
Kauã Pereira <borgeskaua1210@gmail.com>
2026-02-27 15:22:10 -0300

    feat: adiciona classe VariaveisBoolean
    - Demonstra uso do tipo primitivo boolean
    - Explica valores possiveis: true e false
    - Apresenta exemplo simples com status de usuario

---
+++ src/com/kaua/praticando/java/basico/programacaobasica/variaveis/VariaveisBoolean.java
@@ -0,0 +1,19 @@
+package com.kaua.praticando.java.basico.programacaobasica.variaveis;
+
+public class VariaveisBoolean {
+
+    public static void main(String[] args) {
+
+        /*
+         * BOOLEAN
+         * Só aceita dois valores:
+         * true  ou false
+         */
+
+        boolean ativo = true;
+        boolean bloqueado = false;
+
+        System.out.println("Usuário ativo     : " + ativo);
+        System.out.println("Usuário bloqueado : " + bloqueado);
+    }
+}
