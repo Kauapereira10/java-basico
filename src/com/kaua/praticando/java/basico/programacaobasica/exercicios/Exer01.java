831b281ec8f9bc027a8238a1742a39a33304b2fd
Kauã Pereira <borgeskaua1210@gmail.com>
2026-03-04 15:10:32 -0300

    feat: adiciona exercício 01 com Hello World em Java

---
+++ src/com/kaua/praticando/java/basico/programacaobasica/exercicios/Exer01.java
@@ -0,0 +1,7 @@
+package com.kaua.praticando.java.basico.programacaobasica.exercicios;
+
+public class Exer01 {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
