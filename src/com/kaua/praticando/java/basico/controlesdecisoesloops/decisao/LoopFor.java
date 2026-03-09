6ed23e66de83ddd0a316c288997fb30a12e45b73
Kauã Pereira <borgeskaua1210@gmail.com>
2026-03-09 12:57:02 -0300

    feat: adiciona exemplo de loop for com incremento e decremento
    O primeiro exemplo realiza uma contagem crescente de 0 até 5.
    O segundo exemplo demonstra uma contagem decrescente de 5 até 0.

---
+++ src/com/kaua/praticando/java/basico/controlesdecisoesloops/decisao/LoopFor.java
@@ -0,0 +1,24 @@
+package com.kaua.praticando.java.basico.controlesdecisoesloops.decisao;
+
+public class LoopFor {
+	public static void main(String[] args) {
+		
+		// incremento
+		for(int i=0; i <= 5; i++) {
+			System.out.println("i tem valor: " + i);
+		}
+		
+		System.out.println("-------------------");
+		
+		// decremento
+		for (int i=5; i >= 0; i--) {
+			System.out.println("i tem valor: " + i);
+		}
+		
+		/*
+		 * for (inicializacao; condicao; atualizacao) {
+		 * 		bloco de codigo
+		 *  }
+		 * */
+	}
+}
