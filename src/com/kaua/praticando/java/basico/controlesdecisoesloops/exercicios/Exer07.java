b81b5584b023948c7706af7919c21ccf5410562a
Kauã Pereira <borgeskaua1210@gmail.com>
2026-03-09 13:00:34 -0300

    feat: adiciona exercício para identificar o maior e o menor entre três números

---
+++ src/com/kaua/praticando/java/basico/controlesdecisoesloops/exercicios/Exer07.java
@@ -0,0 +1,38 @@
+package com.kaua.praticando.java.basico.controlesdecisoesloops.exercicios;
+
+import java.util.Scanner;
+
+public class Exer07 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Digite o primeiro numero:");
+		int num1 = sc.nextInt();
+		System.out.println("Digite o segundo numero:");
+		int num2 = sc.nextInt();
+		System.out.println("Digite o terceiro numero:");
+		int num3 = sc.nextInt();
+		
+		if(num1 > num2 && num1 > num3) {
+			System.out.println("O numero " + num1 + " é o maior");
+		}else if (num2 > num1 && num2 > num3) {
+			System.out.println("O numero " + num2 + " é o maior");
+		}else if (num3 > num1 && num3 > num2){
+			System.out.println("O numero " + num3 + " é o maior");
+		}else {
+			System.out.println("Os numeros sao iguais");
+		}
+		
+		if(num1 < num2 && num1 < num3) {
+			System.out.println("O numero " + num1 + " é o menor");
+		}else if (num2 < num1 && num2 < num3) {
+			System.out.println("O numero " + num2 + " é o menor");
+		}else if (num3 < num1 && num3 < num2){
+			System.out.println("O numero " + num3 + " é o menor ");
+		}else {
+			System.out.println("Os numeros sao iguais");
+		}
+		
+		sc.close();
+	}
+}
