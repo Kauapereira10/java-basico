7ba177874cd2898716b2f35949ae24064bed4129
Kauã Pereira <borgeskaua1210@gmail.com>
2026-03-11 14:10:05 -0300

    feat: exemplo de uso do break em loop com condição múltipla de 7

---
+++ src/com/kaua/praticando/java/basico/controlesdecisoesloops/breakcontinue/BreakEContinue.java
@@ -0,0 +1,29 @@
+package com.kaua.praticando.java.basico.controlesdecisoesloops.breakcontinue;
+
+import java.util.Scanner;
+
+public class BreakEContinue {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Entre com um número");
+		int num = sc.nextInt();
+		
+		System.out.println("Entre com um limite");
+		int max = sc.nextInt();
+		
+		for (int i = num; i < max; i++) {
+			System.out.println(i);
+			
+			
+			if(i % 7 == 0) {
+				System.out.println("O valor de i é " + i);
+				break;
+			}
+			
+			
+		}
+		
+		sc.close();
+	}
+}
