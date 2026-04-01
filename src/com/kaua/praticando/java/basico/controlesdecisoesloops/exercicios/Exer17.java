c1cbaeabefff0de25819c163b3dc9d2ee0ea21f7
Kauã Pereira <borgeskaua1210@gmail.com>
2026-04-01 11:54:31 -0300

    feat: adicionar verificação de ano bissexto
    A lógica segue as regras:
    - Divisível por 400 -> bissexto
    - Divisível por 4 e não por 100 -> bissexto
    - Caso contrário -> não bissexto
    
    Também inclui uso de Scanner para leitura de dados e fechamento do recurso.

---
+++ src/com/kaua/praticando/java/basico/controlesdecisoesloops/exercicios/Exer17.java
@@ -0,0 +1,21 @@
+package com.kaua.praticando.java.basico.controlesdecisoesloops.exercicios;
+
+import java.util.Scanner;
+
+public class Exer17 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Digite o ano:");
+		int ano = sc.nextInt();
+		
+		if((ano % 400 == 0) || (ano % 4 == 0 && ano % 100 != 0)) {
+			System.out.println(ano + " é um ano Bissexto!");
+		} else {
+			System.out.println(ano + " não é um ano Bissexto!");
+		}
+
+		sc.close();
+	}
+}
